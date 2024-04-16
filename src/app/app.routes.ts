e24fd0d722e96077917be389d4614c50113ea96c
Stina Persson <stpe1901@student.miun.se>
2024-04-16 11:16:44 +0200

    fixat routing och navigering med RouterLink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConvertComponent } from './convert/convert.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+//routing för undersidorna
+export const routes: Routes = [ 
+    { path: 'home', component: HomeComponent}, 
+    { path: 'convert', component: ConvertComponent}, 
+    { path: 'about', component: AboutComponent}, 
+    { path: '', redirectTo: '/home', pathMatch: 'full'} 
+];
