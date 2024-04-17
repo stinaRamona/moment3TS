22c731268904d610e820f63300ca2796a8867b6f
Stina Persson <stpe1901@student.miun.se>
2024-04-17 11:50:01 +0200

    Fixat formulär med funktioner för att konvertera från enheter. Behöver klicka på knappen två gånger för att det ska funka. Fixas.

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.css'
 })
 export class ConvertComponent {
+  //Properties för formulär
+  lengthInput: number = 0;  
+  resultLength: number = 0; 
+  tempInput: number = 0; 
+  resultTemp: number = 0; 
 
+ //metod för att konvertera meter till fot och vice versa
+  convertLengthMtoF(){
+    this.resultLength = this.lengthInput * 3.281
+    } 
+
+  convertLengthFtoM(){
+    this.resultLength = this.lengthInput / 3.281
+  }
+ //metod för att konvertera celsius till farenheit och vice versa
+  convertDegreesCtoF(){
+    this.resultTemp = this.tempInput + 32 * 5 / 9 
+  } 
+
+  convertDegreesFtoC(){
+    this.resultTemp = this.tempInput - 32 / 5 * 9 
+  }
 }
