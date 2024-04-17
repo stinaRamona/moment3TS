bdc4731d3bf7c860cd0ee2ba0184e1bca785fe7b
Stina Persson <stpe1901@student.miun.se>
2024-04-17 15:40:39 +0200

    lagt in information i om samt fixat responsivitet

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -11,26 +11,5 @@ import { ConvertTempComponent } from '../convert-temp/convert-temp.component';
   styleUrl: './convert.component.css'
 })
 export class ConvertComponent {
-  //Properties för formulär
-  lengthInput: number = 0;  
-  resultLength: number = 0; 
-  tempInput: number = 0; 
-  resultTemp: number = 0; 
 
- //metod för att konvertera meter till fot och vice versa
-  convertLengthMtoF(): void {
-    this.resultLength = this.lengthInput * 3.281;
-    } 
-
-  convertLengthFtoM(): void {
-    this.resultLength = this.lengthInput / 3.281;
-  }
- //metod för att konvertera celsius till farenheit och vice versa
-  convertDegreesCtoF(): void{
-    this.resultTemp = this.tempInput * 9 / 5 + 32;
-  } 
-
-  convertDegreesFtoC(): void{
-    this.resultTemp = (this.tempInput - 32) * 5 / 9; 
-  }
 }
